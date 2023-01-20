885e36d76b8a21954dd1682764ec0d1a5f3b4396
Nina <Nina@DESKTOP-UT00UHS>
2023-01-20 15:28:48 +0200

    Add project files.

---
+++ Summator/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator
+{
+    internal class Class1
+    {
+    }
+}
