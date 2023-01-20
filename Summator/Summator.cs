3053adb6621b87e780675f28f43530627a682ea9
Nina <n.i.velkova@gmail.com>
2023-01-20 17:36:46 +0200

    change the code

--- Summator/Summator.cs
+++ Summator/Summator.cs
@@ -8,18 +8,22 @@ namespace Summator
 {
     public static class Summator
     {
+       
         public static int Sum(int[] arr)
         { 
             
         int sum = arr[0];
         for (int i = 1; i < arr.Length; i++)
-        sum += arr[i];
+            {
+                sum += arr[i];
+            }
+        
         return sum;
         }
         public static void Test_SumTwoNumbers()
 
         {
-        if (Sum(new int[] { 1, 2 }) != 3)
+        if (Sum(new int[] { 1, 5 }) != 3)
             {
                 throw new Exception("1+2 != 3");
             }
